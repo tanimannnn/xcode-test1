d550d6d6c7cd3e36d24c187b58c9125971764362
大谷 真広 <motani@oogai-no-MacBook-Air.local>
2012-09-01 13:23:36 +0900

    Initial Commit

---
+++ ButtonEx/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ButtonEx
+//
+//  Created by 真広 大谷 on 12/09/01.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ButtonEx/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ButtonEx
+//
+//  Created by 真広 大谷 on 12/09/01.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
